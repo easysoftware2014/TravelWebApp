da64f99f1154fb3f6b606967c1ff98864b512523
easysoftware2014 <ayandapatrick@gmail.com>
2019-06-11 15:29:40 +0200

    removing files from commit

---
+++ TravelWebApp.Repository/Criteria/GetBudgetByUserIdCriteria.cs
@@ -0,0 +1,23 @@
+﻿using NHibernate;
+using NHibernate.Criterion;
+using TravelWebApp.Domain.Entities;
+using TravelWebApp.Repository.Contracts;
+
+namespace TravelWebApp.Repository.Criteria
+{
+    public class GetBudgetByUserIdCriteria : ICriteriaSpecification<Budget>
+    {
+        private readonly long _id;
+        public GetBudgetByUserIdCriteria(long id)
+        {
+            _id = id;
+        }
+        public ICriteria Criteria(ISession session)
+        {
+            var criteria = session.CreateCriteria(typeof(Budget));
+            criteria.Add(Restrictions.Eq("Id", _id));
+
+            return criteria;
+        }
+    }
+}
